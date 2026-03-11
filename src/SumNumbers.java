e1b99af700ee5fa51156e19a61630673f629e6e4
jerry <jerry2000jerry2010@gmail.com>
2026-03-11 20:13:14 +0000

    Add SumNumbers program

---
+++ src/SumNumbers.java
@@ -0,0 +1,12 @@
+public class SumNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        
+        // Add numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        
+        System.out.println("Sum of 1 to 100: " + sum);
+    }
+}
